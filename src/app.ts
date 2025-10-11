339b0bf2d8daa59d7d44bdf8121d8ac2bab344a6
CounselJS <okpabicounsel@gmail.com>
2025-10-11 01:11:36 +0100

    added  local port

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ app.use(morgan('dev'));
 app.use(helmet());
 
 const corsOptions = {
-  origin: ['http://localhost:5500', 'https://arrarat-designs.onrender.com'],
+  origin: ['http://localhost:5500', 'http://localhost:5501', 'https://arrarat-designs.onrender.com'],
   methods: 'GET, PUT, POST, DELETE, OPTIONS',
   credentials: true,
 };
